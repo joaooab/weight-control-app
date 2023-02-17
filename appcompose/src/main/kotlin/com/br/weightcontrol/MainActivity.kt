190a55ba302168db641b55a826c061490dca19d5
joaofreitas <joaoovfreitas@gmail.com>
2023-02-16 22:55:16 -0300

    feat: remove statuss bar

--- appcompose/src/main/kotlin/com/br/weightcontrol/MainActivity.kt
+++ appcompose/src/main/kotlin/com/br/weightcontrol/MainActivity.kt
@@ -11,12 +11,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.br.weightcontrol.designsystem.theme.WeightcontrolTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             WeightcontrolTheme {
                 // A surface container using the 'background' color from the theme
--- core/design-system/src/main/java/com/br/weightcontrol/designsystem/theme/Theme.kt
+++ core/design-system/src/main/java/com/br/weightcontrol/designsystem/theme/Theme.kt
@@ -94,13 +94,13 @@ fun WeightcontrolTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
+//    val view = LocalView.current
+//    if (!view.isInEditMode) {
+//        SideEffect {
+//            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+//            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+//        }
+//    }
 
     MaterialTheme(
         colorScheme = colorScheme,

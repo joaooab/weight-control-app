86aad47080b0b5688f18daee98ce90d6c41697b3
joaofreitas <joaoovfreitas@gmail.com>
2023-02-17 09:30:16 -0300

    feat: created WeiIcons

---
+++ core/design-system/src/main/java/com/br/weightcontrol/designsystem/icon/WeiIcons.kt
@@ -0,0 +1,25 @@
+package com.br.weightcontrol.designsystem.icon
+
+import androidx.annotation.DrawableRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.History
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+object WeiIcons {
+    val Home = Icons.Filled.Home
+    val HomeBorder = Icons.Outlined.Home
+    val History = Icons.Filled.History
+    val HistoryBorder = Icons.Outlined.History
+    val Settings = Icons.Filled.Settings
+    val SettingsBorder = Icons.Outlined.Settings
+}
+
+sealed class Icon {
+    data class ImageVectorIcon(val imageVector: ImageVector) : Icon()
+    data class DrawableResourceIcon(@DrawableRes val id: Int) : Icon()
+}

eb33922f03dd65428547d7d8993e9470e1dec1bc
joaofreitas <joaoovfreitas@gmail.com>
2023-02-23 23:15:00 -0300

    refactor: added navigation in feature pluggin

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -17,8 +17,10 @@
 import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -32,8 +34,12 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 }
             }
 
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
             dependencies {
                 add("implementation", project(":core:design-system"))
+
+                add("implementation", libs.findLibrary("androidx.navigation.compose").get())
             }
         }
     }

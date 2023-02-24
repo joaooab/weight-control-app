901040f0efdcbe756284809e91bc72c519b21aeb
joaofreitas <joaoovfreitas@gmail.com>
2023-02-23 22:59:29 -0300

    build: added pluggins in features

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -18,12 +18,13 @@ import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply {
-                apply("weighcontrol.android.library")
+                apply("weightcontrol.android.library")
             }
             extensions.configure<LibraryExtension> {
                 defaultConfig {
@@ -31,21 +32,9 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 }
             }
 
-//            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
-
-//            dependencies {
-//                add("implementation", project(":core:model"))
-//                add("implementation", project(":core:ui"))
-//                add("implementation", project(":core:designsystem"))
-//                add("implementation", project(":core:data"))
-//                add("implementation", project(":core:common"))
-//                add("implementation", project(":core:domain"))
-//
-//                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
-//                add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
-//
-//                add("implementation", libs.findLibrary("kotlinx.coroutines.android").get())
-//            }
+            dependencies {
+                add("implementation", project(":core:design-system"))
+            }
         }
     }
 }

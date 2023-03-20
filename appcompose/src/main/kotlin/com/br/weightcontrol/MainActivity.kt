01f31316f060a5a572a93226c19396f02a3fbad5
joaofreitas <joaoovfreitas@gmail.com>
2023-03-20 00:00:48 -0300

    refactor: removed experimental annotation

--- appcompose/src/main/kotlin/com/br/weightcontrol/MainActivity.kt
+++ appcompose/src/main/kotlin/com/br/weightcontrol/MainActivity.kt
@@ -3,17 +3,11 @@ package com.br.weightcontrol
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.br.weightcontrol.designsystem.theme.WeiTheme
 import com.br.weightcontrol.ui.WeiApp
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

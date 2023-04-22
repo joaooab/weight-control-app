e0e6da5360b3b9aea38ac1819dc90b94ac47708c
joaofreitas <joaoovfreitas@gmail.com>
2023-04-22 16:53:52 -0300

    refactor: create date extension and removed unused extension

--- core/util/src/main/java/com/br/weightcontrol/util/DateExtension.kt
+++ core/util/src/main/java/com/br/weightcontrol/util/DateExtension.kt
@@ -1,24 +1,12 @@
 package com.br.weightcontrol.util
 
-import kotlinx.datetime.Clock
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.todayIn
-import java.text.SimpleDateFormat
-import java.util.*
+import kotlinx.datetime.*
 
 private const val DATE_PATTERN = "yyyy-MM-dd"
 private const val DATE_PATTERN_BR = "dd/MM/yyyy"
 
-fun today() = Clock.System.todayIn(TimeZone.currentSystemDefault())
+fun today() = Clock.System.todayIn(TimeZone.UTC)
 fun todayAsString() = today().toString()
-fun stringToDate(date: String) = LocalDate.parse(date)
-
-fun parseOldDateToNewDate(date: String): String {
-    val oldDate = SimpleDateFormat(
-        DATE_PATTERN_BR,
-        Locale.getDefault()
-    ).parse(date) ?: return ""
-
-    return SimpleDateFormat(DATE_PATTERN, Locale.getDefault()).format(oldDate)
-}
+fun String.toLocalDate() = LocalDate.parse(this)
+fun Long.toLocalDate() = Instant.fromEpochMilliseconds(this).toLocalDateTime(TimeZone.UTC).date
+fun LocalDate.toLong() = this.atStartOfDayIn(TimeZone.UTC).toEpochMilliseconds()
--- core/util/src/test/java/com/br/weightcontrol/util/DateExtensionKtTest.kt
+++ core/util/src/test/java/com/br/weightcontrol/util/DateExtensionKtTest.kt
@@ -5,9 +5,4 @@ import org.junit.Test
 
 internal class DateExtensionKtTest {
 
-    @Test
-    fun `Given old date When parse to new date Then return expected`() {
-        assert(parseOldDateToNewDate("27/02/2023") == "2023-02-27")
-        assert(parseOldDateToNewDate("26/02/2023") == "2023-02-26")
-    }
 }

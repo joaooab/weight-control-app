95ce02b9d8283babbd66127b604ab275ace9581e
Joao Freitas <joaoovfreitas@gmail.com>
2023-08-15 07:34:10 -0300

    feat: create apply gender component in profile

--- feature/profile/src/main/java/br/com/weightcontrol/profile/ProfileRoute.kt
+++ feature/profile/src/main/java/br/com/weightcontrol/profile/ProfileRoute.kt
@@ -7,13 +7,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -23,26 +24,40 @@ import androidx.compose.ui.unit.dp
 import com.br.weightcontrol.designsystem.component.WeiButton
 import com.br.weightcontrol.designsystem.component.WeiTopAppBar
 import com.br.weightcontrol.designsystem.theme.WeiTheme
+import com.br.weightcontrol.model.Gender
 import com.br.weightcontrol.profile.R
+import com.br.weightcontrol.ui.GenderSelection
+import com.br.weightcontrol.ui.WeiDatePickerField
+import com.br.weightcontrol.ui.rememberBirthdayDatePickerState
+import com.br.weightcontrol.util.dateValidatorLowerThanToday
+import com.br.weightcontrol.util.onChangeWithLimit
+import com.br.weightcontrol.util.today
+import kotlinx.datetime.LocalDate
 import org.koin.androidx.compose.koinViewModel
 
+// TODO: Form validation https://proandroiddev.com/input-validation-in-jetpack-compose-e99c18b44fe3 
 
 @Composable
 fun ProfileRoute(
     modifier: Modifier = Modifier,
     viewModel: ProfileViewModel = koinViewModel(),
 ) {
-    var inputName by rememberSaveable { mutableStateOf("") }
-    var inputAge by rememberSaveable { mutableStateOf("") }
-    var inputHeight by rememberSaveable { mutableStateOf("") }
+    var inputName by remember { mutableStateOf("") }
+    var inputHeight by remember { mutableStateOf("") }
+    var inputBirthday by remember { mutableStateOf(today()) }
+    var selectedGender by remember { mutableStateOf(Gender.MALE) }
+    val datePickerState = rememberBirthdayDatePickerState()
 
     ProfileScreen(
         selectedName = { inputName },
-        selectedAge = { inputAge },
         selectedHeight = { inputHeight },
+        selectedBirthday = { inputBirthday },
+        selectedGender = { selectedGender },
         onNameChanged = { inputName = it },
-        onAgeChanged = { inputAge = it },
-        onHeightChanged = { inputHeight = it },
+        onBirthdayChanged = { inputBirthday = it },
+        onHeightChanged = { inputHeight = onChangeWithLimit(it, 3) },
+        onGenderChanged = { selectedGender = it },
+        datePickerState = datePickerState,
         onSave = viewModel::save,
         modifier = modifier
     )
@@ -51,14 +66,18 @@ fun ProfileRoute(
 @Composable
 fun ProfileScreen(
     selectedName: () -> String,
-    selectedAge: () -> String,
     selectedHeight: () -> String,
+    selectedBirthday: () -> LocalDate,
+    selectedGender: () -> Gender,
     onNameChanged: (String) -> Unit,
-    onAgeChanged: (String) -> Unit,
+    onBirthdayChanged: (LocalDate) -> Unit,
     onHeightChanged: (String) -> Unit,
+    onGenderChanged: (Gender) -> Unit,
+    datePickerState: DatePickerState,
     modifier: Modifier = Modifier,
-    onSave: (String, String, String) -> Unit
+    onSave: (String, Int, LocalDate) -> Unit
 ) {
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -74,32 +93,41 @@ fun ProfileScreen(
                 .padding(top = 16.dp),
             value = selectedName(),
             onValueChange = { onNameChanged(it) },
-            label = { Text(stringResource(id = R.string.name)) }
+            label = { Text(stringResource(id = R.string.name)) },
+            isError = selectedName().isBlank()
         )
         OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 16.dp),
             value = selectedHeight(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
             onValueChange = { onHeightChanged(it) },
             label = { Text(stringResource(id = R.string.height)) }
         )
-        OutlinedTextField(
+        WeiDatePickerField(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 16.dp),
-            value = selectedAge(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            onValueChange = { onAgeChanged(it) },
-            label = { Text(stringResource(id = R.string.age)) }
+            value = selectedBirthday(),
+            datePickerState = datePickerState,
+            onValueChange = { onBirthdayChanged(it) },
+            dateValidator = dateValidatorLowerThanToday
+        )
+        Text(
+            text = stringResource(id = R.string.gender_select),
+            modifier = modifier.padding(top = 16.dp)
+        )
+        GenderSelection(
+            onGenderSelected = { onGenderChanged(it) },
+            selectedGender = selectedGender(),
         )
         Spacer(modifier = Modifier.weight(1f))
         WeiButton(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 24.dp),
-            onClick = { onSave(selectedName(), selectedAge(), selectedHeight()) }
+            onClick = { onSave(selectedName(), selectedHeight().toInt(), selectedBirthday()) }
         ) {
             Text(text = stringResource(id = R.string.save))
         }

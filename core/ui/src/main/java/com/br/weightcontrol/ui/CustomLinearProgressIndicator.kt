966f49dd8f0a726d24dcbb3e826ba0ec3de70e7a
Joao Freitas <joaoovfreitas@gmail.com>
2023-12-12 07:39:46 -0300

    feature: add progress text

--- core/ui/src/main/java/com/br/weightcontrol/ui/CustomLinearProgressIndicator.kt
+++ core/ui/src/main/java/com/br/weightcontrol/ui/CustomLinearProgressIndicator.kt
@@ -4,23 +4,30 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.br.weightcontrol.util.toPercent
 
 @Composable
 fun CustomLinearProgressIndicator(
@@ -35,17 +42,30 @@ fun CustomLinearProgressIndicator(
         targetValue = progress,
         animationSpec = tween(durationMillis = 2000, easing = FastOutSlowInEasing), label = ""
     )
-    Box(
-        modifier = modifier
-            .clip(clipShape)
-            .background(backgroundColor)
-            .height(8.dp)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
     ) {
         Box(
-            modifier = Modifier
-                .background(progressColor)
-                .fillMaxHeight()
-                .fillMaxWidth(progressAnimation)
+            modifier = modifier
+                .clip(clipShape)
+                .background(backgroundColor)
+                .weight(1f)
+                .height(8.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(progressColor)
+                    .fillMaxHeight()
+                    .fillMaxWidth(progressAnimation)
+            )
+        }
+        Text(
+            text = (progressAnimation * 100).toPercent(),
+            fontWeight = FontWeight.Bold
         )
     }
     LaunchedEffect(targetProgress) {
---
+++ core/util/src/main/java/com/br/weightcontrol/util/NumberExtensions.kt
@@ -0,0 +1,3 @@
+package com.br.weightcontrol.util
+
+fun Number.toPercent() = "${this.toInt()}%"

31792969d684161689a6cab6c2a32c1372f8e1d2
joao freitas <joaoovfreitas@gmail.com>
2024-03-23 14:39:36 -0300

    improvement: update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.br.weightcontrol"
-        versionCode = 1
+        versionCode = 10
         versionName = "2.0.0" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
